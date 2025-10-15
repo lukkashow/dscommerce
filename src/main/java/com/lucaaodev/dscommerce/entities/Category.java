ce065083dcf942877f9b749cbf46f61b1768641d
Adryan Lucas Morais Ribeiro <me.lucasmorais@outlook.com>
2025-10-15 17:51:30 -0300

    Equals and Hashcodes

--- src/main/java/com/lucaaodev/dscommerce/entities/Category.java
+++ src/main/java/com/lucaaodev/dscommerce/entities/Category.java
@@ -4,6 +4,7 @@ package com.lucaaodev.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -45,4 +46,18 @@ public class Category {
     public Set<Product> getProducts() {
         return products;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 }
+
