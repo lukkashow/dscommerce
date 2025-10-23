4e1c62ed24399e30035682eedb03a585ada9b270
Adryan Lucas Morais Ribeiro <me.lucasmorais@outlook.com>
2025-10-22 21:06:48 -0300

    GET product by id

---
+++ src/main/java/com/lucaaodev/dscommerce/services/ProductService.java
@@ -0,0 +1,32 @@
+package com.lucaaodev.dscommerce.services;
+
+import com.lucaaodev.dscommerce.dto.ProductDTO;
+import com.lucaaodev.dscommerce.entities.Product;
+import com.lucaaodev.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id){
+         // Optional<Product> result = repository.findById(id);  //BUSCOU NO BANCO O PRODUTO COM ESSE ID E GRAVA EM RESULT
+        // Product product = result.get(); //PEGO_TODO O PRODUTO
+        // ProductDTO dto = new ProductDTO(product); //CONVERTO ELE PARA UM DTO
+        // return dto; //RETORNO DTO
+
+        Product product = repository.findById(id).get();
+        return new ProductDTO(product);
+
+
+
+    }
+
+}
