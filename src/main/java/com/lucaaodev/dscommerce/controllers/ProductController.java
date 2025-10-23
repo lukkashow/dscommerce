06149979d6913ac5ba7ebf52a12a0dac17fcace6
Adryan Lucas Morais Ribeiro <me.lucasmorais@outlook.com>
2025-10-23 20:23:24 -0300

    GET Products paged

--- src/main/java/com/lucaaodev/dscommerce/controllers/ProductController.java
+++ src/main/java/com/lucaaodev/dscommerce/controllers/ProductController.java
@@ -3,10 +3,14 @@ package com.lucaaodev.dscommerce.controllers;
 import com.lucaaodev.dscommerce.dto.ProductDTO;
 import com.lucaaodev.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -15,10 +19,17 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable){
+        return service.findAll(pageable);
+    }
+
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id){
         return service.findById(id);
 
     }
+
+
 
 }
